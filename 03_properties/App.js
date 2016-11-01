fd9b5b6c48d74ea6c65615c40e8b976a4f6c5cc4
Javier Aguila <sjaca10@gmail.com>
2016-10-31 19:09:23 -0600

    Starting properties lesson

---
+++ 03_properties/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>{this.props.txt}</h1>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App txt="this is the props text" />, document.getElementById('app')
+);
