2fad429f2795d2eddf5f00eac58f351408197bf6
Javier Aguila <sjaca10@gmail.com>
2016-11-01 12:44:45 -0600

    Finishing using react properties lesson

--- 03_properties/App.js
+++ 03_properties/App.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render() {
+        let txt = this.props.txt;
         return (
-            <h1>{this.props.txt}</h1>
+            <h1>{txt}</h1>
         );
     }
 }
 
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired,
+}
+
+App.defaultProps = {
+    txt: 'this is the default txt',
+}
+
 ReactDOM.render(
-    <App txt="this is the props text" />, document.getElementById('app')
+    <App cat={5} txt="this is the props value" />, document.getElementById('app')
+    // In case that the property is not defined, the defaultProps object is used
+    // <App cat={5} />, document.getElementById('app')
 );
