0645928f571cbea2023b4a2788a09fb6fae129f2
Javier Aguila <sjaca10@gmail.com>
2016-10-31 18:22:10 -0600

    Hello world react component

---
+++ 01_intro/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Component with Stateless
+class App extends React.Component {
+    render() {
+        return <h1>Hello Guys</h1>
+    }
+}
+
+// Stateless component
+// const App = () => <h1>Hello Eggheads</h1>
+
+export default App;
